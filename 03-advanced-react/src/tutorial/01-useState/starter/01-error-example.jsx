599de61821f2b4069ea3deda4570f5bcb6e2a4d6
lolodev <horaciodev1706@gmail.com>
2024-03-29 20:37:51 -0300

    01 useState 01 error example

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,24 @@
+const Counter = () => {};
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+	let count = 0;
+	const handleClick = () => {
+		count = count + 1;
+		console.log(count);
+		return count;
+	};
+	return (
+		<div>
+			<h2>{count}</h2>
+			<button
+				type='button'
+				className='btn'
+				onClick={handleClick}
+			>
+				click me
+			</button>
+		</div>
+	);
 };
 
 export default ErrorExample;
