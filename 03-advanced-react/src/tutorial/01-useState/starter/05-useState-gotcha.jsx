5758409b231debb7d95be1811169936c1bb3b992
lolodev <horaciodev1706@gmail.com>
2024-03-30 16:05:06 -0300

    useState gotcha

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,55 @@
+import { useState } from 'react';
+
+//! counter example
+/* const UseStateGotcha = () => {
+	const [value, setValue] = useState(0);
+
+	const handleClick = () => {
+		setValue((currentState) => {
+			const newState = currentState + 1;
+			return newState;
+		});
+	};
+
+	return (
+		<>
+			<h2>useState "gotcha"</h2>
+			<h3>{value}</h3>
+			<button
+				className='btn'
+				onClick={handleClick}
+			>
+				Click Me!
+			</button>
+		</>
+	);
+}; */
+
+//! setTimeout example
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+	const [value, setValue] = useState(0);
+
+	const handleClick = () => {
+		setTimeout(() => {
+			console.log('clicked the button');
+			setValue((currentState) => {
+				return currentState + 1;
+			});
+		}, 3000);
+	};
+
+	return (
+		<>
+			<h2>useState "gotcha"</h2>
+			<h3>{value}</h3>
+			<button
+				className='btn'
+				onClick={handleClick}
+			>
+				Click Me!
+			</button>
+		</>
+	);
 };
 
 export default UseStateGotcha;
