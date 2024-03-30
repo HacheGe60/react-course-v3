ffcc1cbf948b72e1cb1c9c14e3d9cb18d56fbb9d
lolodev <horaciodev1706@gmail.com>
2024-03-30 00:37:00 -0300

    useState Object

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,38 @@
 import { useState } from 'react';
+import { data } from '../../../data';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const [person, setPerson] = useState({
+		name: 'peter',
+		age: 24,
+		hobby: 'read books',
+	});
+
+	const displayRandomPerson = () => {
+		const randomPerson = data[Math.floor(Math.random() * data.length)];
+		setPerson({
+			name: randomPerson.name,
+			age: randomPerson.age,
+			hobby: randomPerson.hobby,
+		});
+	};
+
+	const { name, age, hobby } = person;
+
+	return (
+		<>
+			<h2>useState object example</h2>
+			<h3>{name}</h3>
+			<h3>{age}</h3>
+			<h4>Enjoys: {hobby}</h4>
+			<button
+				className='btn'
+				onClick={displayRandomPerson}
+			>
+				Click me
+			</button>
+		</>
+	);
 };
 
 export default UseStateObject;
