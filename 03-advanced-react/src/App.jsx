1164036010863915e61d9b9cad173dde941e1a16
lolodev <horaciodev1706@gmail.com>
2024-04-01 19:35:08 -0300

    multiple inputs

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import UserChallenge from './tutorial/06-forms/starter/02-user-challenge';
+import UserChallenge from './tutorial/06-forms/starter/03-multiple-inputs';
 function App() {
 	return (
 		<div className='container'>
