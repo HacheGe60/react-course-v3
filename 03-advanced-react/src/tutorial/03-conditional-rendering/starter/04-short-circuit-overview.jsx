eda59ca3cd5d385f753b3f969e9b29ba830b178f
lolodev <horaciodev1706@gmail.com>
2024-04-01 12:55:09 -0300

    Short Circuit Overview

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,22 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+	//* falsy
+	const [text, setText] = useState('');
+	//* truthy
+	const [name, setName] = useState('susan');
+
+	const codeExample = text || 'hello world';
+
+	return (
+		<>
+			{/* if(someCondition){"won't work"} */}
+			<h4>Falsy OR : {text || 'hello world'}</h4>
+			<h4>Falsy AND {text && 'hello world'}</h4>
+			<h4>Truthy OR {name || 'hello you'}</h4>
+			<h4>Truthy AND {name && 'hello you'}</h4>
+			{codeExample}
+		</>
+	);
 };
 export default ShortCircuitOverview;
