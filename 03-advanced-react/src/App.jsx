e97db7f52ed35da967b6e946a164ea1ed835f8f0
lolodev <horaciodev1706@gmail.com>
2024-04-01 19:14:23 -0300

    user challenge

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import List from './tutorial/06-forms/starter/01-controlled-inputs';
+import UserChallenge from './tutorial/06-forms/starter/02-user-challenge';
 function App() {
 	return (
 		<div className='container'>
-			<List />
+			<UserChallenge />
 		</div>
 	);
 }
--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,21 +1,74 @@
-const UserChallenge = () => {
-  return (
-    <div>
-      <form className='form'>
-        <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input type='text' className='form-input' id='name' />
-        </div>
+import { useState } from 'react';
+import { data } from '../../../data';
 
-        <button type='submit' className='btn btn-block'>
-          submit
-        </button>
-      </form>
-      {/* render users below */}
-    </div>
-  );
+const users = [...data];
+
+const UserChallenge = () => {
+	const [users, setUsers] = useState(data);
+	const [name, setName] = useState('');
+	console.log(users);
+
+	let id = Date.now();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!name) return;
+		const newUser = { id, name };
+		const updatedUsers = [...users, newUser];
+		setUsers(updatedUsers);
+		setName('');
+	};
+
+	const removeUser = (id) => {
+		const updatedUsers = users.filter((user) => user.id !== id);
+		setUsers(updatedUsers);
+	};
+
+	return (
+		<div>
+			<form
+				className='form'
+				onSubmit={handleSubmit}
+			>
+				<h4>Add User</h4>
+				<div className='form-row'>
+					<label
+						htmlFor='name'
+						className='form-label'
+					>
+						name
+					</label>
+					<input
+						type='text'
+						className='form-input'
+						id='name'
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+
+				<button
+					type='submit'
+					className='btn btn-block'
+				>
+					submit
+				</button>
+			</form>
+			<h2>users</h2>
+			{users.map((user) => {
+				return (
+					<div key={user.id}>
+						<h4>{user.name}</h4>
+						<button
+							className='btn'
+							onClick={() => removeUser(user.id)}
+						>
+							remove
+						</button>
+					</div>
+				);
+			})}
+		</div>
+	);
 };
 export default UserChallenge;
