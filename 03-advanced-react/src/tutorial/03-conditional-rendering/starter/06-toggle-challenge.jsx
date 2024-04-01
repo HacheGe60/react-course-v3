4663876f60c224667e5806e1bc61b67fb1defb7f
lolodev <horaciodev1706@gmail.com>
2024-04-01 13:43:42 -0300

    toggle challenge

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,22 @@
+import { useState, useEffect } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+	const [value, setValue] = useState(false);
+
+	const text = value ? 'Se prendió la luz' : 'Se apagó la luz';
+	return (
+		<>
+			<h2>toggle challenge</h2>
+			{/* //toggle */}
+			<button
+				className='btn'
+				onClick={() => setValue(!value)}
+			>
+				{value ? 'on' : 'off'}
+			</button>
+			<h4>{text}</h4>
+		</>
+	);
 };
 
 export default ToggleChallenge;
