8f402ecc891b30b2deefcb350c7fcc2cdea6f2e6
lolodev <horaciodev1706@gmail.com>
2024-04-01 22:30:41 -0300

    fetch data

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import UserChallenge from './tutorial/08-custom-hooks/starter/01-toggle';
+import UserChallenge from './tutorial/08-custom-hooks/starter/02-fetch-data';
+import { data } from './data';
 function App() {
 	return (
 		<div className='container'>
