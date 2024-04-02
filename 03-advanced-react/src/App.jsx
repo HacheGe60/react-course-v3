024ff7835a6bbfec8523710f7becef340297c44c
lolodev <horaciodev1706@gmail.com>
2024-04-01 21:56:43 -0300

    custom hooks

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import UserChallenge from './tutorial/07-useRef/starter/01-useRef-basics';
+import UserChallenge from './tutorial/08-custom-hooks/starter/01-toggle';
 function App() {
 	return (
 		<div className='container'>
