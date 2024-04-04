85b6a25e66dfe517050fb5ba8728f384e7518b1b
lolodev <horaciodev1706@gmail.com>
2024-04-04 20:00:55 -0300

    hooks

--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/Person.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/Person.jsx
@@ -1,8 +1,14 @@
-const Person = ({ name }) => {
-  return (
-    <div>
-      <h4>{name}</h4>
-    </div>
-  );
+const Person = ({ name, removePerson, id }) => {
+	return (
+		<div>
+			<h4>{name}</h4>
+			<button
+				onClick={() => removePerson(id)}
+				className='btn'
+			>
+				remove
+			</button>
+		</div>
+	);
 };
 export default Person;
